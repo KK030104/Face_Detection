b4008bdbd75cff546cdabf4cf701cb265407ac3a
KK030104 <109041736+KK030104@users.noreply.github.com>
2023-07-31 22:17:11 +0530

    Update face_detection.py

--- Face_Detection/face_detection.py
+++ Face_Detection/face_detection.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('frontal_face.xml')
 
-img = cv2.imread('rugved.jpg')
+img = cv2.imread('manya3.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
